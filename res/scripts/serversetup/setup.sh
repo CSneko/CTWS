556909ba6ab795d7afd2543518f9bfe6f448d90e
CrystalNeko <minecrafthetun@outlook.com>
2023-09-29 14:52:21 +0800

    Update setup.sh

--- res/scripts/serversetup/setup.sh
+++ res/scripts/serversetup/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://api.purpurmc.org/v2/purpur/1.20.1/latest/download purpur.jar
+wget -O purpur.jar https://api.purpurmc.org/v2/purpur/1.20.1/latest/download 
 wget https://w.csk.asia/res/scripts/serversetup/必读.txt
 wget https://w.csk.asia/res/scripts/serversetup/eula.txt
 wget https://w.csk.asia/res/scripts/serversetup/start.sh
