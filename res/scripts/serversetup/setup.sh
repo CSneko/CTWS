cab58b1aa52535a597be5eadb78abee6bb419d7d
CrystalNeko <minecrafthetun@outlook.com>
2023-09-29 14:46:15 +0800

    a

---
+++ res/scripts/serversetup/setup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+wget https://api.purpurmc.org/v2/purpur/1.20.1/latest/download purpur.jar
+wget https://w.csk.asia/res/scripts/serversetup/必读.txt
+wget https://w.csk.asia/res/scripts/serversetup/eula.txt
+wget https://w.csk.asia/res/scripts/serversetup/start.sh
