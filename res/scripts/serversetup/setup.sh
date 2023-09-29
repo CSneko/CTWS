869c01c3b91bd0e83ad497f7c211bc55bd05cb1a
CrystalNeko <minecrafthetun@outlook.com>
2023-09-29 15:25:51 +0800

    a

--- res/scripts/serversetup/setup.sh
+++ res/scripts/serversetup/setup.sh
@@ -3,3 +3,8 @@ wget -O purpur.jar https://api.purpurmc.org/v2/purpur/1.20.1/latest/download
 wget https://w.csk.asia/res/scripts/serversetup/必读.txt
 wget https://w.csk.asia/res/scripts/serversetup/eula.txt
 wget https://w.csk.asia/res/scripts/serversetup/start.sh
+mkdir plugins
+cd plugins
+wget -O ctLib.jar https://w.csk.asia/res/plugins/ctLib.jar
+wget -O CSNKTools.jar https://w.csk.asia/res/plugins/CSNKTools
+wget -O toNeko.jar https://w.csk.asia/res/plugins/toNeko
